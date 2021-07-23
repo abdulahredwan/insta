240e2ac38a081cf8e90661f1f92ecbf0a37f3f3e
abdulahiredwan <abdulahiredwan@gmial.com>
2021-07-24 00:58:29 +0300

    adding front-end like instagram

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+     <div class="col-3 p-5">
+<img src="/svg/q.jpg" class="rounded-circle" style="height:120px;">
+     </div>
+     <div class="col-9" pt-5>
+       <div ><h1>Abdulahi-Redwan</h1></div>
+         <div class="d-flex">
+             <div class="pr-4">
+               <strong>213</strong>Posts
+             </div>
+             <div class="pr-4">
+               <strong>121K</strong>Followers
+             </div>
+             <div class="pr-4">
+               <strong>2</strong>Followings
+             </div>
+         </div>
+         <div class="pt-4" font-weight-bold >
+           @abdulahiredwan080
+         </div>
+         <div class="">
+           i am Abdulahi Redwan full-stack developer
+         </div>
+         <div class="">
+           <a href="#">Telegarm Account</a>
+         </div>
+       </div>
+
+   </div>
+   <div class="row pt-4">
+     <div class="col-4">
+       <img src="/svg/a.jpg" style="height:400px;"class="w-100" >
+     </div>
+
+       <div class="col-4">
+         <img src="/svg/e.jpg" style="height:400px;"class="w-100" >
+       </div>
+
+         <div class="col-4">
+           <img src="/svg/q.jpg" style="height:400px;"class="w-100" >
+         </div>
+   </div>
+</div>
+@endsection
